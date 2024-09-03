4d007b7bf3383347905a04d14d6aa9f0fcd3c977
JuanDa <robayoolaya@gmail.com>
2024-09-03 07:50:44 -0500

    Registro y enlace

---
+++ Iniciar sesion/Registrarse/inicioRegistro.js
@@ -0,0 +1,9 @@
+///precarga del css para disminuir fouc///
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'estiloRegistro.css';
+link.onload = function() {
+    document.body.style.visibility = 'visible';
+    document.body.style.opacity = '1';
+};
+document.head.appendChild(link);
