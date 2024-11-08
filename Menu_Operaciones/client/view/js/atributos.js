bc0b08627a7d1cb9f11678616eb66bbeb1118eb1
JuanDa <robayoolaya@gmail.com>
2024-11-08 06:37:13 -0500

    arreglo fondos inicio

--- Menu_Operaciones/client/view/js/atributos.js
+++ Menu_Operaciones/client/view/js/atributos.js
@@ -1,10 +1,11 @@
     // FONDOS DINAMICOS
 const fondos = [
     // añade la url del fondo aqui
-    '../img/fondoInicio.svg',
-    '../img/fondoInicio2.svg',
-    '../img/fondoInicio3.svg',
-    '../img/fondoInicio4.svg'
+    '/Menu_Operaciones/client/view/img/fondoInicio.svg',
+    '/Menu_Operaciones/client/view/img/fondoInicio2.svg',
+    '/Menu_Operaciones/client/view/img/fondoInicio3.svg',
+    '/Menu_Operaciones/client/view/img/fondoInicio4.svg',
+
   
   ];
   
