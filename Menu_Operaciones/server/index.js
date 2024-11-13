21b55300d3fc8b4045e179b83dd7bbbabf698823
JuanDa <robayoolaya@gmail.com>
2024-11-13 09:21:09 -0500

    pequeñas actualizaciones

--- Menu_Operaciones/server/index.js
+++ Menu_Operaciones/server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const userController = require("./controller/usuariosController");
 const insertUserController = require("./controller/insertarUsuarioController");
+// const deleteUser = require("./controller/usuarioDelete");
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 app.get("/users", userController.getUsers);
-app.get("/users/insert", insertUserController.createUser);
+app.get("/insert", insertUserController.createUser);
+// app.get("/deleteQuery", deleteUser.deleteUser);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
