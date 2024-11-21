f7c2c0384ee39c86c55a659775aabcdde4e0ce7a
Exhelsior <alejandrosilva.82@hotmail.com>
2024-11-21 09:37:54 -0500

    Update buttons.js

--- Menu_Operaciones/client/controller/buttons.js
+++ Menu_Operaciones/client/controller/buttons.js
@@ -2,16 +2,61 @@
 const modalContent = {
     addProduct: 
         `
-        <span id="close" class="close">&times;</span>
         <h1>Producto</h1>
+        <form id="tb-add-producto">
+            <div class="form-content">
+                <label>Nombre</label>
+                <input type="text" placeholder="Nombre del producto" id="name-product">
+            </div>
+            <div class="form-content">
+                <label>Fecha de <br> fabricacion</label>
+                <input type="date" id="fab-date-product">
+            </div>
+            <div class="form-content">
+                <label>Fecha de <br> vencimiento</label>
+                <input type="date" id="venc-date-product">
+            </div>
+            <div class="form-content">
+                <label>Cantidad</label>
+                <input type="number" placeholder="Cantidad" id="cant-product">
+            </div>
+            <div class="boton-add">
+                <button id="btn-form-product" class="add">Agregar Producto</button>
+            </div>
+        </form>
         `,
     
-    AddInsumos: 
+    addInsumos: 
         `
-        <span id="close" class="close">&times;</span>
         <h1>Insumos</h1>
+        <form id="tb-add-insumo">
+            <div class="form-content">
+                <label>Nombre</label>
+                <input type="text" placeholder="Nombre del insumo" id="name-product">
+            </div>
+            <div class="form-content">
+                <label>Fecha de <br> ingreso</label>
+                <input type="date" id="fab-date-insumo">
+            </div>
+            <div class="form-content">
+                <label>Fecha de <br> vencimiento</label>
+                <input type="date" id="venc-date-insumo">
+            </div>
+            <div class="form-content">
+                <label>Cantidad</label>
+                <input type="number" placeholder="Cantidad" id="cant-insumo">
+            </div>
+            <div class="boton-add">
+                <button id="btn-form-product" class="add">Agregar Insumo</button>
+            </div>
+        </form>
+        `,
+
+    addBillProduct:
         `
-}
+        <h1>Factura</h1>
+        `
+};
 
 //eventos botones
 export function listenerButton(openCallbackModal){
@@ -20,6 +65,12 @@ export function listenerButton(openCallbackModal){
     })
 
     document.getElementById("add-insumo").addEventListener("click", () => {
-        openCallbackModal(modalContent.AddInsumos);
+        openCallbackModal(modalContent.addInsumos);
+    })
+}
+
+export function listenerBillButton(openCallbackModal) {
+    document.getElementById("bill-add-product").addEventListener("click", () => {
+        openCallbackModal(modalContent.addBillProduct);
     })
 }
