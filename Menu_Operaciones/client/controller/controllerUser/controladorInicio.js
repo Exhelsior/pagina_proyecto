ad4c1966ea88227522d2ff583a75d07e306695a1
JuanDa <robayoolaya@gmail.com>
2025-04-04 13:17:33 -0500

    correcciones menores

--- Menu_Operaciones/client/controller/controllerUser/controladorInicio.js
+++ Menu_Operaciones/client/controller/controllerUser/controladorInicio.js
@@ -22,13 +22,11 @@ function mostrarInicio() {
     setTimeout(() => {
       vista.mostrarPlantilla("tempInicio", "main-contenido");
       const botonLogin = document.getElementById("mostraMenu");
-      const error = document.createElement("div");
-      const loginUser = document.createElement("div");
+      const error = document.getElementById("errorMessage");
 
       botonLogin.addEventListener("click", async (event) => {
         event.preventDefault();
         error.classList.add("hidden");
-        loginUser.classList.add("hidden");
 
         const email = document.getElementById("email")?.value.trim();
         const contraseña = document.getElementById("contraseña")?.value.trim();
@@ -40,137 +38,123 @@ function mostrarInicio() {
 
         try {
           const response = await fetch("http://localhost:3000/usuario/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              email: email,
-              password: contraseña,
-            }),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: contraseña,
+        }),
           });
 
           const data = await response.json();
           if (data.success) {
-            logeado(`Bienvenido ${data.user.nombre}, redirigiendo...`);
-            setTimeout(() => {
-              window.location.href = "./indexMenu.html";
-            }, 3000);
+        localStorage.setItem('userRole', data.user.rol.id);
+        localStorage.setItem('userName', data.user.nombre);
+        
+        watchError(`Bienvenido ${data.user.nombre}, redirigiendo...`);
+        setTimeout(() => {
+          window.location.href = "./indexMenu.html";
+        }, 3000);
           } else {
-            watchError(data.message || "Error en las credenciales.");
+        watchError(data.message || "Error en las credenciales.");
           }
         } catch (error) {
           console.error("Error en la petición:", error);
-          watchError(
-            "Error en el servidor. Por favor, intente nuevamente más tarde."
-          );
+          watchError("Error en el servidor. Por favor, intente nuevamente más tarde.");
         }
       });
-
-      function watchError(message) {
-        error.textContent = message;
-        error.classList.remove("hidden");
+        }, 100);
       }
+    }
 
-      function logeado(message) {
-        loginUser.textContent = message;
-        loginUser.classList.remove("hidden");
-      }
-    }, 100);
-  }
-}
+    function watchError(message) {
+      const error = document.getElementById("errorMessage");
+      error.textContent = message;
+      error.classList.remove("hidden");
+    }
 
-function mostrarToken() {
-  if (vistaActual !== "token") {
-    vistaActual = "token"; // Cambia la vista activa
-    setTimeout(() => {
+    function mostrarToken() {
+      if (vistaActual !== "token") {
+        vistaActual = "token";
+        setTimeout(() => {
       vista.mostrarPlantilla("tempToken", "main-contenido");
       const botoncito = document.getElementById("showNotificationBtn");
+      const error = document.getElementById("errorMessage");
+
       botoncito.addEventListener("click", async (event) => {
         event.preventDefault();
+        error.classList.add("hidden");
         const email = document.getElementById("tokenEmail")?.value.trim();
 
         if (!email) {
-          alert("Por favor complete todos los campos.");
+          watchError("Por favor complete todos los campos.");
           return;
         }
 
         try {
-          const response = await fetch(
-            "http://localhost:3000/usuario/recovery",
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                email: email,
-              }),
-            }
-          );
+          const response = await fetch("http://localhost:3000/usuario/recovery", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: email }),
+          });
 
           const data = await response.json();
           if (data.success) {
-            alert(`Token enviado a ${email}`);
-            mostrarRecovery(); // Añadido: cambiar a la plantilla recovery después del éxito
+        watchError(`Token enviado a ${email}`);
+        setTimeout(() => mostrarRecovery(), 3000);
           } else {
-            alert(data.message || "Error en las credenciales.");
+        watchError(data.message || "Error al enviar el token.");
           }
         } catch (error) {
           console.error("Error en la petición:", error);
-          alert(
-            "Error en el servidor. Por favor, intente nuevamente más tarde."
-          );
+          watchError("Error en el servidor. Por favor, intente nuevamente más tarde.");
         }
       });
-    }, 100);
-  }
-}
+        }, 100);
+      }
+    }
 
-function mostrarRecovery() {
-  if (vistaActual !== "recovery") {
-    vistaActual = "recovery"; // Cambia la vista activa
-    setTimeout(() => {
+    function mostrarRecovery() {
+      if (vistaActual !== "recovery") {
+        vistaActual = "recovery";
+        setTimeout(() => {
       vista.mostrarPlantilla("tempRecovery", "main-contenido");
       const botoncote = document.getElementById("showNotificationBtn");
+      const error = document.getElementById("errorMessage");
+
       botoncote.addEventListener("click", async (event) => {
-        console.log(tempRecovery);
         event.preventDefault();
-        const recoveryToken = document
-          .getElementById("recoveryToken")
-          ?.value.trim();
-        const newPassword = document
-          .getElementById("newPassword")
-          ?.value.trim();
+        error.classList.add("hidden");
+        const recoveryToken = document.getElementById("recoveryToken")?.value.trim();
+        const newPassword = document.getElementById("newPassword")?.value.trim();
 
         if (!recoveryToken || !newPassword) {
-          alert("Por favor complete todos los campos.");
+          watchError("Por favor complete todos los campos.");
           return;
         }
 
         try {
-          const response = await fetch(
-            "http://localhost:3000/usuario/newPassword",
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                recoveryToken: recoveryToken,
-                newPassword: newPassword,
-              }),
-            }
-          );
+          const response = await fetch("http://localhost:3000/usuario/newPassword", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          recoveryToken: recoveryToken,
+          newPassword: newPassword,
+        }),
+          });
 
           const data = await response.json();
           if (data.success) {
-            alert("Contraseña cambiada correctamente");
-            mostrarInicio(); // cambiar a la plantilla inicio después del éxito
+        watchError("Contraseña cambiada correctamente");
+        setTimeout(() => mostrarInicio(), 3000);
           } else {
-            alert(data.message || "Error en las credenciales.");
+        watchError(data.message || "Error al cambiar la contraseña.");
           }
         } catch (error) {
           console.error("Error en la petición:", error);
-          console.error(
-            "Error en el servidor. Por favor, intente nuevamente más tarde."
-          );
+          watchError("Error en el servidor. Por favor, intente nuevamente más tarde.");
         }
       });
-    }, 100);
-  }
-}
+        }, 100);
+      }
+    }
